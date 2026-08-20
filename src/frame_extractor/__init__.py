221390cbe71e809ccc69df20b3d0067dd201502b
Javier Gonzalez <jgonmor16@gmail.com>
2026-08-20 11:40:51 +0200

    chore(release): v2.0.1

--- src/frame_extractor/__init__.py
+++ src/frame_extractor/__init__.py
@@ -36,7 +36,7 @@ from frame_extractor.extractor import (
 )
 from frame_extractor.ffmpeg_utils import Progress
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 __all__ = [
     # The one thing most callers need.
